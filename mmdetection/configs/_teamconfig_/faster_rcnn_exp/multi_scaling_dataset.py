baa44945471d70f925cc82c6195cc53d72af5333
안세희_T6094 <49268298+seheeAn@users.noreply.github.com>
2024-01-13 00:14:51 +0900

    feat: Faster R-CNN backbone 변경, cascade rpn 실험 (#54)
    * feat: add Faster R-CNN train config
    
    #28
    
    * feat: add Faster R-CNN with cascade rpn
    
    #28
    
    * feat: Faster R-CNN에 multi scaling 실험 추가
    
    #28
    
    * feat: Faster R-CNN cascade rpn, backbone 실험
    
    #28

---
+++ mmdetection/configs/_teamconfig_/faster_rcnn_exp/multi_scaling_dataset.py
@@ -0,0 +1,89 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/data/ephemeral/home/workspace/level2-objectdetection-cv-13/dataset/'
+
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    #dict(type='Resize', img_scale=(512, 512), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='AutoAugment',
+         policies=[
+             [
+                 dict(type='Resize',
+                      img_scale=[(480, 1024), (512, 1024), (544, 1024), (576, 1024),
+                                 (608, 1024), (640, 1024), (672, 1024), (704, 1024),
+                                 (736, 1024), (768, 1024), (800, 1024)],
+                      multiscale_mode='value',
+                      keep_ratio=True)
+             ],
+             [
+                 dict(type='Resize',
+                      img_scale=[(400, 1024), (500, 1024), (600, 1024)],
+                      multiscale_mode='value',
+                      keep_ratio=True),
+                 dict(type='RandomCrop',
+                      crop_type='absolute_range',
+                      crop_size=(384, 600),
+                      allow_negative_crop=True),
+                 dict(type='Resize',
+                      img_scale=[(480, 1024), (512, 1024), (544, 1024),
+                                 (576, 1024), (608, 1024), (640, 1024),
+                                 (672, 1024), (704, 1024), (736, 1024),
+                                 (768, 1024), (800, 1024)],
+                      multiscale_mode='value',
+                      override=True,
+                      keep_ratio=True)
+             ]
+         ]),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'new_train_k-fold-2024/train_fold0.json',
+        img_prefix=data_root,
+        classes=classes,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'new_train_k-fold-2024/valid_fold0.json',
+        img_prefix=data_root,
+        classes=classes,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        classes=classes,
+        pipeline=test_pipeline,
+        # test_mode = True,
+        ))
+
+#evaluation = dict(interval=1, metric='bbox')
